323ac6b6904c349bc7733661639bc3df02c660a6
James Brattin <James Brattin>
2017-02-12 16:48:34 -0600

    .gitignore is now working

--- app/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-
-    app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
-
-};
