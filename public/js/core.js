22134db525735b0bc5de3d7c48d01eec0033f132
Brattin <jb185287@ncr.com>
2017-03-27 16:52:57 -0500

    Removed comment

--- public/js/core.js
+++ public/js/core.js
@@ -5,38 +5,3 @@
         .module('app.core', []);
 
 })();
-
-/*
- * recommend
- * Using function declarations
- * and bindable members up top.
- * 
-function AvengersController(avengersService, logger) {
-    var vm = this;
-    vm.avengers = [];
-    vm.getAvengers = getAvengers;
-    vm.title = 'Avengers';
-
-    activate();
-
-    function activate() {
-        return getAvengers().then(function() {
-            logger.info('Activated Avengers View');
-        });
-    }
-
-    function getAvengers() {
-        return avengersService.getAvengers().then(function(data) {
-            vm.avengers = data;
-            return vm.avengers;
-        });
-    }
-}
-
-
-
-
-
-
-
-*/
