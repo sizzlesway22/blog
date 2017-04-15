431d4c47cf409782adbd712aed9943324cd9caf9
James Brattin <James Brattin>
2017-04-15 16:32:36 -0500

    cleaned it up

--- public/js/core/module.core.js
+++ public/js/core/module.core.js
@@ -2,6 +2,26 @@
     'use strict';
 
     angular
-        .module('app.core', ['ngRoute']);
+        .module('app.core', ['ngRoute'])
+        .config(config);
+
+    function config($routeProvider, $locationProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'home.html',
+                controller: 'HomeController',
+                controllerAs: 'Home'
+            })
+            .when('/dashboard', {
+                templateUrl: 'dashboard.html',
+                controller: 'DashboardController',
+                controllerAs: 'Ctrl'
+            }).otherwise({
+                redirectTo: '/'
+            });
+
+        $locationProvider.html5Mode(true);
+    }
 
 })();
--- public/js/module.app.js
+++ public/js/module.app.js
@@ -3,25 +3,5 @@
 
     angular
         .module('app', ['app.core', 'app.dashboard'])
-        .config(config);
-
-    function config($routeProvider, $locationProvider) {
-
-        $routeProvider
-            .when('/', {
-                templateUrl: 'home.html',
-                controller: 'HomeController',
-                controllerAs: 'Home'
-            })
-            .when('/dashboard', {
-                templateUrl: 'dashboard.html',
-                controller: 'DashboardController',
-                controllerAs: 'Ctrl'
-            }).otherwise({
-                redirectTo: '/'
-            });
-
-        $locationProvider.html5Mode(true);
-    }
 
 })();
