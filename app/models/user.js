bf804139dbf6fe7a45bb782925781e7c5d060f3d
Brattin <jb185287@ncr.com>
2017-04-28 17:17:05 -0500

    changed some passport stuff

--- app/models/user.js
+++ app/models/user.js
@@ -1,57 +1,42 @@
-var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
-var Schema = mongoose.Schema;
+var mongoose = require( 'mongoose' );
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
-var userSchema = mongoose.Schema({
-
-    local            : {
-        username     : { type: String, required: true, unique: true},
-        password     : String,
-        created_at   : Date,
-	    updated_at   : Date
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
-    
-});
-
-userSchema.pre('save', function(next) {
-	var currentDate = new Date();
-
-	this.updated_at = currentDate;
-
-	if (!this.created_at) {
-		this.created_at = currentDate;
-	}
-
-	next();
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
 });
 
 // methods ======================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
-// checking if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
 };
 
 // create the model for users and expose it to our app
--- config/passport.js
+++ config/passport.js
@@ -1,105 +1,28 @@
-var LocalStrategy   = require('passport-local').Strategy;
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
-// load up the user model
-var User            = require('../app/models/user');
-
-// expose this function to our app using module.exports
-module.exports = function(passport) {
-
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
-
-    // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
-        done(null, user._id);
-    });
-
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            done(err, user);
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+  },
+  function(username, password, done) {
+    User.findOne({ email: username }, function (err, user) {
+      if (err) { return done(err); }
+      // Return if user not found in database
+      if (!user) {
+        return done(null, false, {
+          message: 'User not found'
         });
-    });
-
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
-    passport.use('login', new LocalStrategy({
-        passReqToCallback : true
-    },
-    function(req, username, password, done) {
-        console.log(req); 
-        // check in mongo if a user with username exists or not
-        User.findOne({ 'username' :  username }, 
-        function(err, user) {
-            // In case of any error, return using the done method
-            if (err)
-            return done(err);
-            // Username does not exist, log error & redirect back
-            if (!user){
-            console.log('User Not Found with username '+username);
-            return done(null, false);                 
-            }
-            // User exists but wrong password, log the error 
-            if (!User.ValidPassword(user, password)){
-            console.log('Invalid Password');
-            return done(null, false);
-            }
-            // User and password both match, return user from 
-            // done method which will be treated like success
-            return done(null, user);
-        }
-        );
-    }));
-
-    passport.use('signup', new LocalStrategy({
-        passReqToCallback : true
-    },
-    function(req, username, password, done) {
-        console.log(req);
-        findOrCreateUser = function() {
-        // find a user in Mongo with provided username
-        User.findOne({'username':username}, function(err, user) {
-            // In case of any error return
-            if (err){
-                console.log('Error in SignUp: '+err);
-                return done(err);
-            }
-            // already exists
-            if (user) {
-                console.log('User already exists');
-                return done(null, false);
-            } else {
-                // if there is no user with that email
-                // create the user
-                var newUser = new User();
-                // set the user's local credentials
-                newUser.username = username;
-                newUser.password = User.generateHash(password);
-        
-                // save the user
-                newUser.save(function(err) {
-                    if (err) {
-                        console.log('Error in Saving user: '+err);  
-                        throw err;  
-                    } else {
-                        console.log('User Registration succesful');    
-                        return done(null, newUser);
-                    }
-                });
-            }
+      }
+      // Return if password is wrong
+      if (!user.validPassword(password)) {
+        return done(null, false, {
+          message: 'Password is wrong'
         });
-        };
-        
-        // Delay the execution of findOrCreateUser and execute 
-        // the method in the next tick of the event loop
-        process.nextTick(findOrCreateUser);
-    })
-    );
-};
+      }
+      // If credentials are correct, return the user object
+      return done(null, user);
+    });
+  }
+));
